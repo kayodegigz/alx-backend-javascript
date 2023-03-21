d59c0229a273ce309f1842b2b83752975d300565
kayodegigz <kayogunrinde@gmail.com>
2023-03-21 13:46:16 +0100

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const res = [];
+  for (const element of array) {
+    res.push(`${appendString}${element}`);
+  }
+  return res;
+}
