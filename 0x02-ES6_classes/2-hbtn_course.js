92ed811b4cdd38e09cd80b896f0bea6b4cf59897
kayodegigz <kayogunrinde@gmail.com>
2023-03-28 23:33:35 +0100

    implement class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('name must be a String');
+    if (typeof length !== 'number') throw TypeError('length must be a Number');
+    if (!(students instanceof Array)) throw TypeError('students must be an Array');
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw TypeError('students must contain strings');
+      }
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('name must be a String');
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('length must be a Number');
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (!(students instanceof Array)) throw TypeError('students must be an Array');
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw TypeError('students must contain strings');
+      }
+    }
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
