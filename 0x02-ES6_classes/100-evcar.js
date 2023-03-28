27c0d1345454db7bbc735a203d00aa9f74467ee6
kayodegigz <kayogunrinde@gmail.com>
2023-03-29 00:12:22 +0100

    evcar class

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+/* eslint-disable require-jsdoc */
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    const NewCar = super.constructor || super[Symbol.species];
+    return new NewCar();
+  }
+}
+
+export default EVCar;
