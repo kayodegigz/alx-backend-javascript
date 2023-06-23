10242af31f9fb65c74dcb0921db87461e5e35913
kayodegigz <kayogunrinde@gmail.com>
2023-06-23 18:29:24 +0100

    getting commands from stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdout.write('Welcome to Holberton School, what is your name?');
+
+process.stdout.write('/n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
